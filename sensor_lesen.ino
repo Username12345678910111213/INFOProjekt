d3f0ec3dccaec1a3ffffcca2777a38868005477c
Username12345678910111213 <66132780+Username12345678910111213@users.noreply.github.com>
2020-06-01 20:19:59 +0200

    Add files via upload
    sensor_lesen gibt die Werte des LDR über den Seriellen Monitor zurück.

---
+++ sensor_lesen.ino
@@ -0,0 +1,12 @@
+void setup() {
+  // put your setup code here, to run once:
+Serial.begin(9600);
+}
+
+void loop() {
+  int Sensorwert = analogRead(A0);
+  Serial.println(Sensorwert);
+  delay(1000);
+  // put your main code here, to run repeatedly:
+
+}
